7fa3150681b30012d4f8f0bfc46122abcd6e28d5
Davide Bellettini <davide@bellettini.me>
2015-12-12 19:09:36 +0100

    Stub simpleSmoothing function

--- src/RobustHoltWinters.cpp
+++ src/RobustHoltWinters.cpp
@@ -1,27 +1,7 @@
-#include <Rcpp.h>
+#include <Rcpp>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+NumericVector simpleSmoothing(NumericVector series, double alpha, double startValue) {
+    int length = series.length();
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
