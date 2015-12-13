b04735b7d5d3a615f1d768ec27d6535395b7357e
Davide Bellettini <davide@bellettini.me>
2015-12-13 11:54:06 +0100

    Simple smoothing works

--- src/RobustHoltWinters.cpp
+++ src/RobustHoltWinters.cpp
@@ -1,17 +1,18 @@
 #include <Rcpp.h>
 #include <assert.h>
+#include <iostream>
 
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericVector simpleSmoothing(NumericVector series, double alpha, double startValue) {
+NumericVector simpleSmoothing(NumericVector series, const double alpha, const double startValue) {
     if (alpha > 1.0 || alpha < 0.0)
         throw std::range_error("Inadmissible value");
 
-    int length = series.length();
-    double previous = startValue;
+    const int length = series.length();
+    NumericVector result(length);
 
-    NumericVector result = NumericVector::create(length);
+    double previous = startValue;
 
     for (int i = 0; i < length; ++i) {
         result[i] = alpha * series[i] + (1.0 - alpha) * previous;
