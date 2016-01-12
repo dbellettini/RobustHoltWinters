170a5f08abadad521f956c0ae1ab9d9c1698e753
Davide Bellettini <davide@bellettini.me>
2016-01-12 09:17:24 +0100

    Remove debugging instruction

--- tests/Periodic.R
+++ tests/Periodic.R
@@ -6,6 +6,5 @@ test.periodic <- function () {
     series <- ts(frequency = 7, synthetic(0:500))
     model <- RobustHoltWinters(series)
 
-    print(model)
     checkEqualsNumeric(1.0, model$coefficients['s4'], "Seasonal coefficient should be 1.0", 0.001)
 }
