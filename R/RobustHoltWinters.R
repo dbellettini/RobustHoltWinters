396fca7f79667cc0952ff9f1c089345619e8aad5
Davide Bellettini <davide@bellettini.me>
2016-01-20 11:57:14 +0100

    Fix comments

--- R/RobustHoltWinters.R
+++ R/RobustHoltWinters.R
@@ -1,9 +1,6 @@
-#  File src/library/stats/R/HoltWinters.R
-#  Part of the R package, http://www.R-project.org
-#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -14,6 +11,7 @@
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 
+# Robust Holt-Winters based on R source code
 # Originally contributed by David Meyer
 
 RobustHoltWinters <-
