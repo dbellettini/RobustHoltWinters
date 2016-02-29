cdd4388fcbba124ac573311484a6f950f4b707d0
Davide Bellettini <davide.bellettini@onebip.com>
2016-02-29 17:23:38 +0100

    Back to GPLv2

--- src/RobustHoltWinters.cpp
+++ src/RobustHoltWinters.cpp
@@ -7,7 +7,7 @@
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -17,7 +17,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, a copy is available at
- *  http://www.gnu.org/licenses/gpl-3.0.txt
+ *  http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 #include <Rcpp.h>
